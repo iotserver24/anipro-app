5cd6116324941fca2b9de46ac3bcf275d3708aff
R3AP3R edit <147928812+iotserver24@users.noreply.github.com>
2025-11-07 11:27:42 +0530

    feat: Update dependencies and enhance plugin registration for multi-platform support
    - Added new dependencies including device_info_plus, file_picker, flutter_local_notifications, and flutter_tts to improve functionality.
    - Updated pubspec.lock to reflect new versions of existing packages.
    - Enhanced plugin registration for file selection and text-to-speech features across macOS and Windows platforms.
    - Removed unused dependencies from pubspec.yaml to streamline the project.

--- flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import device_info_plus
+import file_picker
+import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_database
+import flutter_local_notifications
+import flutter_tts
 import package_info_plus
 import path_provider_foundation
 import share_plus
@@ -21,9 +26,14 @@ import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
